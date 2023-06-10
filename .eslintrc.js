22dc52a5a5309e4e852326ca4a44de81f0507957
adbc <adrianboczon98@gmail.com>
2023-06-10 07:43:20 +0200

    Add to eslint ignore

--- .eslintrc.js
+++ .eslintrc.js
@@ -10,6 +10,7 @@ module.exports = {
         "plugin:react/recommended",
         "plugin:react-hooks/recommended",
     ],
+    ignorePatterns: ["pnpm-lock.yaml"],
     parser: "@typescript-eslint/parser",
     parserOptions: {
         ecmaVersion: "latest",
