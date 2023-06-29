6e09b2fa1923f748dc16d182502629b96f5dc9d6
adbc <adrianboczon98@gmail.com>
2023-06-29 18:48:12 +0200

    Style changes for list view

--- src/app/events/[id]/ListViewDialog.tsx
+++ src/app/events/[id]/ListViewDialog.tsx
@@ -39,74 +39,93 @@ export const ListViewDialog = forwardRef<Ref, ListViewDialogProps>(
             [allChoices],
         );
 
+        const availabilityColor = {
+            available: "bg-green-500 backdrop-filter bg-opacity-10",
+            maybe_available: "bg-green-900",
+            unavailable: "bg-green-900",
+        };
+
         return (
             <dialog
                 ref={ref}
-                className="p-0 rounded-md max-h-screen"
+                className="p-0 rounded-md h-[128] w-full"
                 open={false}
             >
                 <GlassmorphicPane innerClassName="py-6 px-4">
-                    <div className="flex justify-end mb-4">
-                        <Button
-                            aria-label="close list view button"
-                            type="button"
-                            theme="BASIC"
-                            className="w-9 h-9"
-                        >
-                            <Image
-                                src={closeIcon}
-                                className="cursor-pointer m-auto"
-                                width={24}
-                                height={24}
-                                alt="close modal icon"
-                                onClick={closeModal}
-                            />
-                        </Button>
-                    </div>
-                    <div className="overflow-auto shadow-md sm:rounded-lg">
-                        <table className="w-full text-sm text-left text-gray-400">
-                            <thead className="text-xs uppercase bg-gray-700 text-gray-400">
-                                <tr>
-                                    <th scope="col" className="px-2 py-2">
-                                        &nbsp;
-                                    </th>
-                                    {users.map((user) => (
-                                        <th
-                                            scope="col"
-                                            key={user}
-                                            className="px-2 py-2"
-                                        >
-                                            {user}
-                                        </th>
-                                    ))}
-                                </tr>
-                            </thead>
-                            <tbody>
-                                {Object.entries(allChoices).map(
-                                    ([day, dayChoices]) => (
-                                        <tr
-                                            key={day}
-                                            className="border-b bg-gray-800 border-gray-700"
-                                        >
+                    <div className="flex flex-col">
+                        <div className="flex justify-end mb-4">
+                            <Button
+                                aria-label="close list view button"
+                                type="button"
+                                theme="BASIC"
+                                className="w-9 h-9"
+                            >
+                                <Image
+                                    src={closeIcon}
+                                    className="cursor-pointer m-auto"
+                                    width={24}
+                                    height={24}
+                                    alt="close modal icon"
+                                    onClick={closeModal}
+                                />
+                            </Button>
+                        </div>
+                        <div className="relative basis-96">
+                            <div className="absolute overflow-auto inset-0 shadow-md sm:rounded-lg">
+                                <table className="table-fixed w-full text-center text-sm text-gray-400">
+                                    <thead className="text-xs uppercase text-gray-400 h-10 bg-gray-900 backdrop-filter bg-opacity-10">
+                                        <tr>
                                             <th
-                                                scope="row"
-                                                className="px-2 py-2 font-medium text-gray-400 whitespace-nowrap"
+                                                scope="col"
+                                                className="px-2 py-2"
                                             >
-                                                {day}
+                                                &nbsp;
                                             </th>
                                             {users.map((user) => (
-                                                <td
-                                                    key={day + user}
+                                                <th
+                                                    scope="col"
+                                                    key={user}
                                                     className="px-2 py-2"
                                                 >
-                                                    {dayChoices[user]}
-                                                </td>
+                                                    {user}
+                                                </th>
                                             ))}
                                         </tr>
-                                    ),
-                                )}
-                            </tbody>
-                        </table>
+                                    </thead>
+                                    <tbody>
+                                        {Object.entries(allChoices).map(
+                                            ([day, dayChoices]) => (
+                                                <tr
+                                                    key={day}
+                                                    className="border-b border-gray-900 bg-gray-100 backdrop-filter bg-opacity-10"
+                                                >
+                                                    <th
+                                                        scope="row"
+                                                        className="px-2 py-2 font-medium text-gray-400 whitespace-nowrap"
+                                                    >
+                                                        {day}
+                                                    </th>
+                                                    {users.map((user) => (
+                                                        <td
+                                                            key={day + user}
+                                                            className={`px-2 py-2 ${
+                                                                availabilityColor[
+                                                                    dayChoices[
+                                                                        user
+                                                                    ]
+                                                                ]
+                                                            }`}
+                                                        >
+                                                            {dayChoices[user]}
+                                                        </td>
+                                                    ))}
+                                                </tr>
+                                            ),
+                                        )}
+                                    </tbody>
+                                </table>
+                            </div>
+                        </div>
                     </div>
                 </GlassmorphicPane>
             </dialog>
