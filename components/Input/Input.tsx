4911028c0b7bb8eed01e660153efd9bd999c2b7e
adbc <adrianboczon98@gmail.com>
2023-07-02 07:25:40 +0200

    Format

--- components/Input/Input.tsx
+++ components/Input/Input.tsx
@@ -10,17 +10,13 @@ type Ref = HTMLInputElement;
 const baseInputStyle =
     "border border-neutral-900 focus:outline-cyan-200 block rounded-md bg-zinc-900 bg-clip-padding backdrop-filter backdrop-blur-3xl bg-opacity-40";
 
-export const Input = forwardRef<Ref, InputProps>(
-    function Input(
-        { children, className, ...props },
-        ref,
-    ) {
-        if (typeof ref === "function") {
-            throw new Error("Unexpected ref type");
-        }
+export const Input = forwardRef<Ref, InputProps>(function Input(
+    { children, className, ...props },
+    ref,
+) {
+    if (typeof ref === "function") {
+        throw new Error("Unexpected ref type");
+    }
 
-        return (
-            <input {...props} className={twMerge(baseInputStyle, className)} />
-        );
-    },
-);
+    return <input {...props} className={twMerge(baseInputStyle, className)} />;
+});
