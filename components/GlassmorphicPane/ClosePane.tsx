282c6a6c0e9897cc1e13cced1515d323c99f8afb
adbc <adrianboczon98@gmail.com>
2023-12-17 00:38:27 +0100

    Add close pane button

---
+++ components/GlassmorphicPane/ClosePane.tsx
@@ -0,0 +1,32 @@
+import Image from "next/image";
+
+import closeIcon from "~/public/close.svg";
+
+import { Button } from "../Button/Button";
+import { ReactProps } from "~/typescript";
+
+type ClosePaneProps = ReactProps & {
+    closeModal: () => void;
+};
+
+export function ClosePaneButton({ closeModal }: ClosePaneProps) {
+    return (
+        <div className="flex justify-end mb-4">
+            <Button
+                aria-label="close list view button"
+                type="button"
+                theme="BASIC"
+                className="w-9 h-9 rounded-full border-none"
+                onClick={closeModal}
+            >
+                <Image
+                    src={closeIcon}
+                    className="m-auto"
+                    width={24}
+                    height={24}
+                    alt="close modal icon"
+                />
+            </Button>
+        </div>
+    );
+}
